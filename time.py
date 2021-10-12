99a3d1a9a231aed86552e743e043e9eaa50d5115
fasfda432 <87399731+fasfda432@users.noreply.github.com>
2021-10-11 21:37:03 -0700

    Create time.py

---
+++ time.py
@@ -0,0 +1,11 @@
+import time
+alive = True #@param {type:'boolean'}
+temps = 0
+sec = 3600
+while(temps < 43200):
+  if(temps == sec):
+    sec += 3600
+    x = (temps/60)/60
+    print("It's been ",x," mining hour...")
+  time.sleep(1)
+  temps += 1
